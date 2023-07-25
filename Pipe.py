042953fe7ef4b40f3a5cb3cc5315e34587341a96
Siddharth Movaliya <siddraj1992@gmail.com>
2023-07-25 19:42:04 +0530

    adjust height randomness for the pipes

--- Pipe.py
+++ Pipe.py
@@ -21,7 +21,7 @@ class Pipe:
         self.set_height()
 
     def set_height(self):
-        self.height = random.randrange(50, 450)
+        self.height = random.randrange(50, 365)
         self.top = self.height - self.PIPE_TOP.get_height()
         self.bottom = self.height + self.GAP
 
